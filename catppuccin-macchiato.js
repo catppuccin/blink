9706a7f60963b67662f7a4602b68070d45970a9a
Rithika Silva <rithikasilva@outlook.com>
2022-06-06 20:03:16 -0400

    Background colours updated to match new style guide.

--- catppuccin-macchiato.js
+++ catppuccin-macchiato.js
@@ -1,4 +1,4 @@
 t.prefs_.set('color-palette-overrides', ["#5b6078", "#ee99a0", "#a6da95", "#eed49f", "#7dc4e4", "#f5bde6", "#91d7e3", "#f4dbd6", "#c6a0f6", "#ee99a0", "#a6da95", "#eed49f", "#7dc4e4", "#f5bde6", "#91d7e3", "#cad3f5"]);
 t.prefs_.set('foreground-color', "#cad3f5");
-t.prefs_.set('background-color', "#1e2030");
+t.prefs_.set('background-color', "#24273a");
 t.prefs_.set('cursor-color', "#f0c6c6");
--- catppuccin-mocha.js
+++ catppuccin-mocha.js
@@ -1,4 +1,4 @@
 t.prefs_.set('color-palette-overrides', ["#585b70", "#eba0ac", "#a6e3a1", "#f9e2af", "#74c7ec", "#f5c2e7", "#89dceb", "#f5e0dc", "#cba6f7", "#eba0ac", "#a6e3a1", "#f9e2af", "#74c7ec", "#f5c2e7", "#89dceb", "#cdd6f4"]);
 t.prefs_.set('foreground-color', "#cdd6f4");
-t.prefs_.set('background-color', "#181825");
+t.prefs_.set('background-color', "#1e1e2e");
 t.prefs_.set('cursor-color', "#f2cdcd");
